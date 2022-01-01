99dcb1bb5dd16922a12f21b940838294267783d2
JYoonJong <dbswhd493@naver.com>
2022-01-01 15:04:54 +0900

    Array practice 1

---
+++ git test/src/Arraypratice1.java
@@ -0,0 +1,12 @@
+
+public class Arraypratice1 {
+
+	public static void main(String[] args) {
+		String[] members = { "홍길동" , "둘리", "마이콜" };
+		for(int i =0; i < members.length; i++) {
+			String member = members[i];
+			System.out.println(member + "이 상담을 받았습니다.");
+		}
+	}
+
+}
