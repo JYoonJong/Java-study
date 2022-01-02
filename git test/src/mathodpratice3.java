879524a31416be89790738e146bc9c6cdb7dd212
JYoonJong <dbswhd493@naver.com>
2022-01-02 20:15:15 +0900

    메소드 연습3 - 입력값~ 입력값 출력하는 방법

---
+++ git test/src/mathodpratice3.java
@@ -0,0 +1,16 @@
+
+public class mathodpratice3 {
+
+	public static void number(int a , int b) {
+		int i = a;
+		while( i < b ){
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+	public static void main(String[] args) {
+		number(1,8);
+	}
+	
+}
