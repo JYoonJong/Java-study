f5c9821fae3784fbb537bfe8ecc06675fca39b57
JYoonJong <dbswhd493@naver.com>
2022-01-02 20:14:37 +0900

    메소드 연습2 - 0 ~ 입력값까지 출력하는 방법

---
+++ git test/src/mathodpratice2.java
@@ -0,0 +1,16 @@
+// 입력값을 주는법
+public class mathodpratice2 {
+
+	public static void number(int a) {
+		int i = 0;
+		while( i < a ){
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+	public static void main(String[] args) {
+		number(8);
+	}
+	
+}
