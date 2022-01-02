36d81fce62f8aca439c5cc249c8291048b171081
JYoonJong <dbswhd493@naver.com>
2022-01-02 19:53:23 +0900

    메소드 연습 - 중복으로 반복하는 값 만들기

---
+++ git test/src/mathodpratice1.java
@@ -0,0 +1,21 @@
+
+public class mathodpratice1 {
+
+	public static void number() {
+		int i = 0;
+		while( i <10 ) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+	public static void main(String[] args) {
+		number();
+		number();
+		number();
+		number();
+		number();
+		
+	}
+	
+}
