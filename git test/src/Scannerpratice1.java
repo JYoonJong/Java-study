5390512c30323fea219dd7cce0f46834b010f177
JYoonJong <dbswhd493@naver.com>
2022-01-02 22:00:34 +0900

    Scanner 입출력 - 중복값 사용 ( 계속해서 입력할 수 있음 hasNextInt() )

---
+++ git test/src/Scannerpratice1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Scannerpratice1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()){
+			System.out.println(sc.nextInt()*1000);
+		}
+
+	}
+
+}
